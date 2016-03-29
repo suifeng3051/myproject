04d5f8075894486b5226fb1cb1357a3038955662
lwf <lwf@zizizizizi.com>
2016-03-29 16:57:44 +0800

    菜单跳转问题

--- src/main/webapp/statics/js/manual.js
+++ src/main/webapp/statics/js/manual.js
@@ -34,4 +34,18 @@ $(document).ready(function(){
       var theme = $(e.target).html(); // 之前激活按钮显示的关键字
       console.log(theme);
     });
+
+    // DataTable关闭一些默认配置
+    $.extend($.fn.dataTable.defaults, {
+        searching: false,
+        ordering: false
+    });
+
+    //  给API配置DIV绑定dataTable插件
+    var instanceDetailTable = $("#instanceTable").DataTable({
+        paging: false,
+        language: {"infoEmpty": "","info": ""}
+        //info: false
+        //info: false
+    });
 });
