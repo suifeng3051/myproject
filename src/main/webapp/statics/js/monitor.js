8663289cb2d57e93e9159c4d391b2053b743e4b7
lwf <lwf@zizizizizi.com>
2016-03-30 11:59:31 +0800

    独立菜单文件

--- src/main/webapp/statics/js/monitor.js
+++ src/main/webapp/statics/js/monitor.js
@@ -1,47 +1,5 @@
 $(function(){
 
-    // 当前激活按钮绑定事件
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-      var theme = $(e.target).html(); // 当前激活按钮显示的关键字
-      var env = $("#env").val();
-      var group = $("#currentGroup").val();
-      if("Release" == theme){
-        window.location.href = "release?env=" + env;
-      } else if("Home" == theme) {
-        window.location.href = "apilist?env=" + env;
-      } else if("Config" == theme) {
-        window.location.href = "createapi?env=" + env + "&group=" + group;
-      } else if("LogView" == theme) {
-        window.location.href = "pipelog?env=" + env;
-      } else if("Users" == theme) {
-       window.location.href = "user?env=" + env;
-      } else if("Manual" == theme) {
-        window.location.href = "manual?env=" + env;
-      } else if("Cache" == theme) {
-        window.location.href = "cachemanage?env=" + env;
-      } else if("Instance" == theme) {
-         window.location.href = "instancedetail?env=" + env;
-      } else if("RecoverApi" == theme) {
-        window.location.href = "recoverapi?env=" + env;
-      } else if ("Client" == theme) {
-            window.location.href = "oauthclient?env=" + env;
-       }
-
-
-    });
-
-    // 跳转到帮助页面
-    $(".help").on("click", function(e){
-        e.preventDefault();
-        var env = $("#env").val();
-        location.href = "manual?env=" + env;
-    });
-
-    // 之前激活按钮的绑定事件
-    $('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
-      var theme = $(e.target).html(); // 之前激活按钮显示的关键字
-    });
-
     $(".needTip").tooltip();
 
     // 按日期区间查询监控数据
