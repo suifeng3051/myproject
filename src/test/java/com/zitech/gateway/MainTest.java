3dcb2a7976774827724dbf3af4e790a79b4f8e98
鲍和映 <bhy@zizizizizi.com>
2016-05-07 09:29:03 +0800

    improve compact json string

--- src/test/java/com/zitech/gateway/MainTest.java
+++ src/test/java/com/zitech/gateway/MainTest.java
@@ -1,39 +1,53 @@
 package com.zitech.gateway;
 
+import com.zitech.gateway.gateway.PipeHelper;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Stack;
 
 public class MainTest {
+
     public static String compactJson(String json) {
         if (StringUtils.isEmpty(json))
             return "";
 
         char[] charArray = json.toCharArray();
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
 
-        int i = 0;
-        char c;
+        int i = 0, count = 0, slash = 0;
+        char c, b = 0;
         while (i < charArray.length) {
             c = charArray[i];
             switch (c) {
                 case '"':
                 case '\'':
-                case '\\':
-                    if (stack.size() % 2 == 1) {
-                        stack.push(1);
-                        sb.append(c);
+                    if (count % 2 == 0) {
+                        b = c;
+                        ++count;
+                    } else {
+                        if (c == b) {
+                            if (slash % 2 == 0) {
+                                ++count;
+                                b = 0;
+                            } else {
+                                ++slash; // tread \" as \\ to ++slash
+                            }
+                        }
                     }
+                    sb.append(c);
+                    break;
+                case '\\':
+                    ++slash;
+                    sb.append(c);
                     break;
                 case ' ':
                 case '\t':
                 case '\r':
                 case '\n':
-                    if (stack.size() % 2 == 1)
+                    if (count % 2 == 1)
                         sb.append(c);
                     break;
                 default: {
@@ -45,7 +59,6 @@ public class MainTest {
 
         return sb.toString();
     }
-
     public static void main(String[] args) throws IOException {
         FileInputStream fisStruct = new FileInputStream("/Users/bobo/Desktop/json.json");
         String s = IOUtils.toString(fisStruct);
